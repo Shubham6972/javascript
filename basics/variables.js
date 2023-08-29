4ad87acce6b9daed49ccc2039e242df39310c65d
Shubham Sonkusare <121250963+Shubham6972@users.noreply.github.com>
2023-08-29 17:37:06 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 122335
+let accountEmail = "shubham2gmail.com"
+var accountPassword = "12345"
+accountCity = "thane"
+
+//const cannot be changed 
+//accountId = 2
+console.log(accountId);
