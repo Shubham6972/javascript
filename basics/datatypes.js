e08f61c01725af03ac8e13fa4c9990488e5a7636
Shubham6972 <shubhamsonkusare18698@gmail.com>
2023-08-31 11:11:53 +0530

    Datatypes Learned

---
+++ basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs and not browser .it will show error
+
+let name = "Shubham"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+// number => 2^53
+// bigint => not used generally
+// string => " " => prefer in double quotes
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// object 
+
+console.log(typeof "Shubham") // gives type of variables
+console.log(typeof null)      // null is an object
+console.log(typeof undefined)  // undefined type
