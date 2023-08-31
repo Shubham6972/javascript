8207d1611a16e3d2d368698865054cdb0a8399a1
Shubham6972 <shubhamsonkusare18698@gmail.com>
2023-08-31 12:11:11 +0530

    learning operations

--- basics/03_conversionOpertion.js
+++ basics/03_conversionOpertion.js
@@ -22,5 +22,16 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+// ****************************************Operations************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+console.log(2**2)  // 2^2
+
+
+
+
