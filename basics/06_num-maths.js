89ac01acbcdbbd3a03abb075ea99a603da43f7d0
Shubham6972 <shubhamsonkusare18698@gmail.com>
2023-09-22 12:26:49 +0530

    learn maths

--- basics/06_num-maths.js
+++ basics/06_num-maths.js
@@ -1,1 +1,31 @@
-const score = 
+const score = 400;
+//console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString);
+// console.log(typeof balance);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 229.8966;
+// console.log(otherNumber.toPrecision(3));
+
+const hundred = 100000;
+// console.log(hundred.toLocaleString('en-IN'));
+
+//+++++++++++++++++++Maths+++++++++++++++++++++++++++++++
+console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,3,6,1));
+
+console.log((Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
